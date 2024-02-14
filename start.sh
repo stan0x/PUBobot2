f717309889378233132d90281869a20dfa4e7fdd
stan0x <stan0x@gmail.com>
2024-02-14 22:40:53 +0100

    put respawn timer in function.

--- start.sh
+++ start.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
 LOG_FILE="pubobot.log"
-sleeptimer=5
+SLEEP_TIMER=5
 
 log() {
+    local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
     local message="$1"
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> "$LOG_FILE"
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message"
+    echo "$timestamp - $message" | tee -a "$LOG_FILE"
 }
 
 run_bot() {
     python3 PUBobot2.py
     local exit_code=$?
     log "PUBobot2.py exited with code $exit_code"
+
     if [ $exit_code -eq 0 ]; then
         log "PUBobot2.py exited successfully. Respawning.."
-        
-        # Start countdown timer
-        count=$sleeptimer
-        while [ $count -gt 0 ]; do
-            echo -ne "\rRespawning in $count seconds..."
-            sleep 1
-            count=$((count - 1))
-        done
-        echo ""  # Print a newline after the countdown
     else
         log "PUBobot2.py crashed with exit code $exit_code. Respawning.."
     fi
+
+    respawn_countdown
+}
+
+respawn_countdown() {
+    local count=$SLEEP_TIMER
+    while [ $count -gt 0 ]; do
+        echo -ne "\rRespawning in $count seconds..."
+        sleep 1
+        count=$((count - 1))
+    done
+    echo ""  # Print a newline after the countdown
 }
 
 handle_error() {
